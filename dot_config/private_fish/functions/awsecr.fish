1d26ddb8999cc4c37d667688379c65fa62fd8e25
cmac4603 <12590206+cmac4603@users.noreply.github.com>
2025-12-16 15:00:08 +0000

    chore(fish): comment out unused k8s and podman settings
    - Commented out local Kubernetes context and API URL variables in config.fish.tmpl for clarity.
    - Disabled podman login commands in awsecr.fish, leaving only docker logins active.

--- dot_config/private_fish/functions/awsecr.fish
+++ dot_config/private_fish/functions/awsecr.fish
@@ -2,8 +2,8 @@
 function awsecr
     echo "Logging into public ECR..."
     aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
-    aws ecr-public get-login-password --region us-east-1 | podman login --username AWS --password-stdin public.ecr.aws
+    # aws ecr-public get-login-password --region us-east-1 | podman login --username AWS --password-stdin public.ecr.aws
     echo -e "\nLogging into private ECR..."
     aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 451479211471.dkr.ecr.us-east-1.amazonaws.com
-    aws ecr get-login-password --region us-east-1 | podman login --username AWS --password-stdin 451479211471.dkr.ecr.us-east-1.amazonaws.com
+    # aws ecr get-login-password --region us-east-1 | podman login --username AWS --password-stdin 451479211471.dkr.ecr.us-east-1.amazonaws.com
 end
