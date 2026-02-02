7c40319bd5d6810b76cee11a59a672a5c32d2116
cmac4603 <12590206+cmac4603@users.noreply.github.com>
2026-02-02 08:50:03 +0000

    feat(plugins): add stay-centered.nvim with custom skip_filetypes
    Trying this out, think I might like it

---
+++ dot_config/nvim/lua/plugins/centered.lua
@@ -0,0 +1,18 @@
+return {
+    'arnamak/stay-centered.nvim',
+    lazy = false,
+    opts = {
+        skip_filetypes = {
+            "codecompanion",
+            "conf",
+            "harpoon",
+            "lazy_backdrop",
+            "lazy",
+            "mason_backdrop",
+            "mason",
+            "oil_preview",
+            "oil",
+            "qf",
+        },
+    }
+}
