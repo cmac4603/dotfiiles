d33dc477d3ecb0c916f447b0b2e64513f76193d4
cmac4603 <12590206+cmac4603@users.noreply.github.com>
2026-02-02 10:47:32 +0000

    feat(centered): disable scroll move in stay-centered.nvim
    Set `allow_scroll_move` to false in the stay-centered.nvim plugin configuration to prevent scrolling from moving the cursor out of the center.

--- dot_config/nvim/lua/plugins/centered.lua
+++ dot_config/nvim/lua/plugins/centered.lua
@@ -2,6 +2,7 @@ return {
     'arnamak/stay-centered.nvim',
     lazy = false,
     opts = {
+        allow_scroll_move = false,
         skip_filetypes = {
             "codecompanion",
             "conf",
