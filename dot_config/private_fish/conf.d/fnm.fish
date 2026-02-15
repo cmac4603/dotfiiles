cf71dea6278aeae43f9249194a7a497432dc9ce0
cmac4603 <12590206+cmac4603@users.noreply.github.com>
2026-02-15 19:42:33 +0000

    feat(fish): add FNM_PATH and update PATH for fnm if directory exists
    Adds logic to set FNM_PATH and prepend it to PATH in the Fish shell
    configuration. Also sources fnm environment if the directory exists,
    improving fnm integration.

--- dot_config/private_fish/conf.d/fnm.fish
+++ dot_config/private_fish/conf.d/fnm.fish
@@ -1,1 +1,8 @@
 status is-interactive && fnm env --use-on-cd --shell fish | source
+
+# fnm
+set FNM_PATH "/home/cmac4603/.local/share/fnm"
+if [ -d "$FNM_PATH" ]
+  set PATH "$FNM_PATH" $PATH
+  fnm env | source
+end
