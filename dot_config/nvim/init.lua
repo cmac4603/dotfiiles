8487c7a9eab6a288ac1e95896bb9230762834d48
cmac4603 <12590206+cmac4603@users.noreply.github.com>
2026-02-25 22:11:21 +0000

    feat(nvim/tmux): migrate window navigation to tmux-navigator plugin
    - Set `vim.g.tmux_navigator_no_mappings = 1` to disable default
      mappings.
    - Remove custom window navigation keymaps from lazy.lua.
    - Update navigation plugin config to use tmux-navigator keymaps and
      commands.
    - Clean up unused tmux-navigator commands.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -6,6 +6,8 @@ vim.loader.enable()
 
 require("nvim-dap-projects").search_project_config()
 
+vim.g.tmux_navigator_no_mappings = 1
+
 vim.g.trailing_whitespace_exclude_filetypes = {
     "alpha",
     "git",
