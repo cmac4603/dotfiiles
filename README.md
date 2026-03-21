31eb177677a12b711a0eab3e538a452ecbe2dbe7
cmac4603 <12590206+cmac4603@users.noreply.github.com>
2026-03-21 19:18:10 +0000

    feat(nvim): switch to markdown-plus.nvim & update localleader
    - Replaced render-markdown.nvim with yousefhadder/markdown-plus.nvim for
      improved Markdown support.
    - Changed Neovim maplocalleader from '\' to ',' for consistency.
    - Cleaned up README.md example config.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Dotfiiles (V2)
 
 ## `~/.config/chezmoi/chezmoi.toml`
+
 ```toml
 [data]
-    burden_api_key_ro = ""
     github_token = ""
 ```
--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -1,15 +1,7 @@
 return {
     {
-        "MeanderingProgrammer/render-markdown.nvim",
-        dependencies = {
-            {
-                "nvim-treesitter/nvim-treesitter",
-                branch = "main",
-            },
-            "nvim-tree/nvim-web-devicons",
-        },
-        ---@module "render-markdown"
-        ---@type render.md.UserConfig
+        "yousefhadder/markdown-plus.nvim",
+        ft = "markdown",
         opts = {},
     },
 }
