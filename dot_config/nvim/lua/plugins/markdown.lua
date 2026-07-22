270ec84b47a5a02f329f1af0eff5eec988e1a8fe
cmac4603 <12590206+cmac4603@users.noreply.github.com>
2026-07-22 13:41:10 +0100

    chore(nvim): update markdown-plus opts

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -5,6 +5,16 @@ return {
         opts = {
             features = {
                 list_management = false,
+                text_formatting = false,
+                links = false,
+                images = false,
+                headers_toc = true,
+                quotes = false,
+                callouts = false,
+                code_block = false,
+                html_block_awareness = false,
+                table = false,
+                footnotes = false,
             },
         },
     },
